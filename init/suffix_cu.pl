860f844d5e2b3eeff7c43bab0f69aad75a9304fe
Hashimoto Junji <hasimoto@k-micro.com>
2012-10-10 13:39:56 +0900

    Modified debugger-option for cuda

--- init/suffix_cu.pl
+++ init/suffix_cu.pl
@@ -1,9 +1,15 @@
 $cc="nvcc";
 $gdb="cuda-gdb";
+
+$debug_opt ="-g -G";
+
 $opt.=" -I/usr/local/cuda/sdk/C/common/inc";
+
 #$opt.=" --compiler-options -fno-inline -Xcompiler -D__builtin_stdarg_start=__builtin_va_start";
 #$opt.=" --compiler-bindir /usr/local/cuda/gcc.bin";
+
 $libs.=" -L/usr/local/cuda/sdk/C/lib -lcutil_x86_64";
+
 $default_header=qq(
 #include <cutil.h>
 #include <cuda_macro.h>
