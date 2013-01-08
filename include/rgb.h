af0bcd5130fe0bdca77d556547830d80a82885b5
junji <junji@maggy.(none)>
2013-01-09 00:34:29 +0900

    Added oprator for rgb

--- include/rgb.h
+++ include/rgb.h
@@ -11,14 +11,16 @@ struct color{
   const T& operator[](int k)const ;
 };
 
-template<class T> T        nrm(const color<T>& a);
-template<class T> color<T> abs(const color<T>& a);
-template<class T> color<T> operator -(const color<T>& a,const color<T>& b);
-template<class T> color<T> operator +(const color<T>& a,const color<T>& b);
-template<class T> color<T> operator *(const color<T>& a,const color<T>& b);
-template<class T> color<T> operator /(const color<T>& a,const color<T>& b);
-template<class T> int      operator ==(const color<T>& a,const color<T>& b);
-template<class T> int      operator !=(const color<T>& a,const color<T>& b);
+
+template<class T> T        my_abs	(const T& a);
+template<class T> T        nrm		(const color<T>& a);
+template<class T> color<T> abs		(const color<T>& a);
+template<class T> color<T> operator -	(const color<T>& a,const color<T>& b);
+template<class T> color<T> operator +	(const color<T>& a,const color<T>& b);
+template<class T> color<T> operator *	(const color<T>& a,const color<T>& b);
+template<class T> color<T> operator /	(const color<T>& a,const color<T>& b);
+template<class T> int      operator ==	(const color<T>& a,const color<T>& b);
+template<class T> int      operator !=	(const color<T>& a,const color<T>& b);
 
 typedef color<int> RGB;
 
@@ -64,6 +66,61 @@ color<T>::operator[](int k)const {
   default:
     return b;
   }
+}
+
+template<class T>
+T
+my_abs(const T& a){
+  return a<0? -a:a;
+}
+
+template<class T>
+T
+nrm(const color<T>& a){
+  color<T> ab=abs(a);
+  return ab.r+ab.g+ab.b;
+}
+
+template<class T>
+color<T>
+abs(const color<T>& a){
+  return color<T>(my_abs(a.r),my_abs(a.g),my_abs(a.b));
+}
+
+template<class T>
+color<T>
+operator -(const color<T>& a,const color<T>& b){
+  return color<T>(a.r-b.r,
+		  a.g-b.g,
+		  a.b-b.b
+		  );
+}
+
+template<class T>
+color<T>
+operator +(const color<T>& a,const color<T>& b){
+  return color<T>(a.r+b.r,
+		  a.g+b.g,
+		  a.b+b.b
+		  );
+}
+
+template<class T>
+color<T>
+operator *(const color<T>& a,const color<T>& b){
+  return color<T>(a.r*b.r,
+		  a.g*b.g,
+		  a.b*b.b
+		  );
+}
+
+template<class T>
+color<T>
+operator /(const color<T>& a,const color<T>& b){
+  return color<T>(a.r/b.r,
+		  a.g/b.g,
+		  a.b/b.b
+		  );
 }
 
 
