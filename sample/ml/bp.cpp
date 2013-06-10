112b5d2c7214f8ad1ccdb2b3f5c9e04bc27a431c
Junji Hashimoto <junji.hashimoto@gmail.com>
2013-06-11 02:22:07 +0900

    update

--- sample/ml/bp.cpp
+++ sample/ml/bp.cpp
@@ -1,49 +1,45 @@
 #!/usr/bin/env c-script
 
-struct BP{
-  Matrix<double> X;
-  Matrix<double> D;
-  Matrix<double> S;
-  Matrix<double> W;
-  Matrix<double> dW;
-  Matrix<double> Y;
-  Matrix<double> Hd;
-  double d;
-  void forward(){
-    S=W*Y;
-    mat_for(Hd)
-      X(r,c)=sigm(S(r,c));
-  }
-  void update(){
+double
+sigm(double v){
+  return 1.0/(1.0+exp(-v));
+}
+const Matrix<double>&
+sigm(const Matrix<double>& X){
+  Matrix<double> a(X.nr,X.nc);
+  mat_for(a)
+    a(r,c)=sigm(X(r,c));
+  return a;
+}
 
-    mat_for(Hd)
-      Hd(r,c)=sigm(S(r,c))*(1-sigm(S(r,c)));
-    dW=-2*ary_mul(ary_mul((D-X),Hd),Y);
+template<int N>
+struct BP{
+  Matrix<double> X[N+1];
+  Matrix<double> D[N];
+  Matrix<double> S[N];
+  Matrix<double> W[N]; 
+  double d;
+  void init(){
+    
   }
-  void update(Matrix<double>& hW,Matrix<double>& hY){
-    mat_for(Hd)
-      Hd(r,c)=sigm(S(r,c))*(1-sigm(S(r,c)));
-    dW=-2*ary_mul(ary_mul(t(hY*hW),Hd),Y);
+  //X[i+1]=sigm(S[i]=W[i]*X[i])
+  const Matrix<double>&
+  forward(const Matrix<double>& Y){
+    X[0]=Y;
+    for(int i=0;i<N;i++){
+      S[i]=W[i]*X[i];
+      X[i+1]=sigm(S);
+    }
+    return X[N];
   }
+  void back(const Matrix<double>& D){
+  }
+
 };
 
 int
 main(){
-  SVM svm;
-  svm.init(9,2);
-  svm.set();
-  int n=100000;
-  for(int i=0;i<100000;i++){
-    svm.update(i,n);
-  }
-  
-  printf("L:\n");
-  svm.L.write(stdout,"%2.2f");
-  printf("w:\n");
-  svm.w.write(stdout,"%2.2f");
-  printf("LY:%f\n",dot(svm.L,svm.Y));
-  printf("cost:%f\n",svm.cost());
-
+  BP<1>  bp;
   
   return 0;
 }
